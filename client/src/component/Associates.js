79a7c09cd2d135648a60d735c2cf19e14dadc110
Ferny <fernc28@outlook.com>
2024-03-20 23:13:25 -0400

    client side - working on associates && service layout. NOT fin

--- client/src/component/Associates.js
+++ client/src/component/Associates.js
@@ -27,90 +27,73 @@ function Associates() {
                 <div className="grid justify-center md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-7 my-10">
                     {/* Card 1 */}
                     <div className="bg-white rounded-lg border shadow-md max-w-xs md:max-w-none overflow-hidden">
-                        <img className="h-56 lg:h-60 w-full object-cover" src="https://images.unsplash.com/photo-1523289217630-0dd16184af8e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8d29tZW4lMjBlbXBvd2VybWVudHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+                        {/* <img className="h-56 lg:h-60 w-full object-cover"
+                            src="" alt="" /> */}
+
+                        {/* avatarr image */}
+                        <div className=" flex justify-center py-5">
+                            <div class=" flex items-center justify-center w-20 h-20 overflow-hidden 
+                            bg-gray-100 rounded-full dark:bg-gray-300">
+                                <span class="font-medium text-gray-300 dark:text-white">JL</span>
+                            </div>
+                        </div>
+
                         <div className="p-3">
-                            <span className="text-sm text-primary">November 19, 2022</span>
+                            <span className="text-sm text-primary">Date</span>
                             <h3 className="font-semibold text-xl leading-6 text-gray-700 my-2">
-                                International Women's Day 2022: Date, history, significance, theme this year
+                                Some Text
                             </h3>
                             <p className="paragraph-normal text-gray-600">
-                                Happy Women's Day 2022: Read on to know all about the history and significance...
+                                Some Text ...
                             </p>
-                            <a className="mt-3 block" href="/">Read More </a>
                         </div>
                     </div>
 
                     {/* Card 2 */}
                     <div className="bg-white rounded-lg border shadow-md max-w-xs md:max-w-none overflow-hidden">
-                        <img className="h-56 lg:h-60 w-full object-cover" src="https://images.unsplash.com/photo-1607748862156-7c548e7e98f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fHdvbWVuJTIwZW1wb3dlcm1lbnR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" />
+                        {/* <img className="h-56 lg:h-60 w-full object-cover"
+                            src="" alt="" /> */}
+
+                        {/* avatarr image */}
+                        <div className=" flex justify-center py-5">
+                            <div class=" flex items-center justify-center w-20 h-20 overflow-hidden 
+                            bg-gray-100 rounded-full dark:bg-gray-300">
+                                <span class="font-medium text-gray-300 dark:text-white">JL</span>
+                            </div>
+                        </div>
+
                         <div className="p-3">
-                            <span className="text-sm text-primary">November 19, 2022</span>
+                            <span className="text-sm text-primary">Date</span>
                             <h3 className="font-semibold text-xl leading-6 text-gray-700 my-2">
-                                International Women's Day 2022: Date, history, significance, theme this year
+                                Some Text
                             </h3>
                             <p className="paragraph-normal text-gray-600">
-                                Happy Women's Day 2022: Read on to know all about the history and significance...
+                                Some Text ...
                             </p>
-                            <a className="mt-3 block" href="/">Read More </a>
                         </div>
                     </div>
 
                     {/* Card 3 */}
                     <div className="bg-white rounded-lg border shadow-md max-w-xs md:max-w-none overflow-hidden">
-                        <img className="h-56 lg:h-60 w-full object-cover" src="https://images.unsplash.com/photo-1637419450536-378d5457abb8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fHdvbWVuJTIwZW1wb3dlcm1lbnR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" />
-                        <div className="p-3">
-                            <span className="text-sm text-primary">November 19, 2022</span>
-                            <h3 className="font-semibold text-xl leading-6 text-gray-700 my-2">
-                                International Women's Day 2022: Date, history, significance, theme this year
-                            </h3>
-                            <p className="paragraph-normal text-gray-600">
-                                Happy Women's Day 2022: Read on to know all about the history and significance...
-                            </p>
-                            <a className="mt-3 block" href="/">Read More </a>
-                        </div>
-                    </div>
-                    {/* Card 4 */}
-                    <div className="bg-white rounded-lg border shadow-md max-w-xs md:max-w-none overflow-hidden">
-                        <img className="h-56 lg:h-60 w-full object-cover" src="https://images.unsplash.com/photo-1621352404112-58e2468993a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fHdvbWVuJTIwZW1wb3dlcm1lbnR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" />
-                        <div className="p-3">
-                            <span className="text-sm text-primary">November 19, 2022</span>
-                            <h3 className="font-semibold text-xl leading-6 text-gray-700 my-2">
-                                International Women's Day 2022: Date, history, significance, theme this year
-                            </h3>
-                            <p className="paragraph-normal text-gray-600">
-                                Happy Women's Day 2022: Read on to know all about the history and significance...
-                            </p>
-                            <a className="mt-3 block" href="/">Read More</a>
-                        </div>
-                    </div>
+                        {/* <img className="h-56 lg:h-60 w-full object-cover"
+                            src="" alt="" /> */}
 
-                    {/* Card 5 */}
-                    <div className="bg-white rounded-lg border shadow-md max-w-xs md:max-w-none overflow-hidden">
-                        <img className="h-56 lg:h-60 w-full object-cover" src="https://images.unsplash.com/photo-1607868894064-2b6e7ed1b324?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fHdvbWVuJTIwZW1wb3dlcm1lbnR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" />
-                        <div className="p-3">
-                            <span className="text-sm text-primary">November 19, 2022</span>
-                            <h3 className="font-semibold text-xl leading-6 text-gray-700 my-2">
-                                International Women's Day 2022: Date, history, significance, theme this year
-                            </h3>
-                            <p className="paragraph-normal text-gray-600">
-                                Happy Women's Day 2022: Read on to know all about the history and significance...
-                            </p>
-                            <a className="mt-3 block" href="/">Read More </a>
+                        {/* avatarr image */}
+                        <div className=" flex justify-center py-5">
+                            <div class=" flex items-center justify-center w-20 h-20 overflow-hidden 
+                            bg-gray-100 rounded-full dark:bg-gray-300">
+                                <span class="font-medium text-gray-300 dark:text-white">JL</span>
+                            </div>
                         </div>
-                    </div>
 
-                    {/* Card 6 */}
-                    <div className="bg-white rounded-lg border shadow-md max-w-xs md:max-w-none overflow-hidden">
-                        <img className="h-56 lg:h-60 w-full object-cover" src="https://images.unsplash.com/photo-1633329712176-4751f52ccc1b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fHdvbWVuJTIwZW1wb3dlcm1lbnR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" />
                         <div className="p-3">
-                            <span className="text-sm text-primary">November 19, 2022</span>
+                            <span className="text-sm text-primary">Date</span>
                             <h3 className="font-semibold text-xl leading-6 text-gray-700 my-2">
-                                International Women's Day 2022: Date, history, significance, theme this year
+                                Some Text
                             </h3>
                             <p className="paragraph-normal text-gray-600">
-                                Happy Women's Day 2022: Read on to know all about the history and significance...
+                                Some Text ...
                             </p>
-                            <a className="mt-3 block" href="/">Read More</a>
                         </div>
                     </div>
                 </div>
