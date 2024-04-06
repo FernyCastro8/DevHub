9dc3990172f5cdfcf36990eabaa85492fc75226b
Ferny <fernc28@outlook.com>
2024-04-05 23:12:09 -0400

    Client side - Associates && service component layout fininshed.

--- client/src/component/Footer.js
+++ client/src/component/Footer.js
@@ -23,7 +23,7 @@ function Footer() {
                 </div>
                 <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
                 <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="/" className="hover:underline">DevHub™</a> . All Rights Reserved.
-                    <p className='text-gray-400'>Website designed by <a className="text-blue-600 block text-sm sm:text-center hover:underline" href="https://fernycastro.com">Ferny Castro</a></p>
+                    <p className='text-gray-400'>Designed by<a className="text-blue-600 block text-sm sm:text-center hover:underline" href="https://fernycastro.com">Ferny Castro</a></p>
                 </span>
             </div>
         </footer>
