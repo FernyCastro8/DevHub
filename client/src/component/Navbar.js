f9b042bcf4adc85bf4fc3a2822b6149cac4b7ea1
Ferny <fernc28@outlook.com>
2024-04-13 22:04:11 -0400

    Client side - navBar Hamburger menu fin

--- client/src/component/Navbar.js
+++ client/src/component/Navbar.js
@@ -38,10 +38,10 @@ function Navbar() {
                 <div className="flex md:order-2 items-center flex-row-reverse space-x-3 md:space-x-0 ">
                     <div className="hidden md:flex md:items-center md:w-auto md:order-1">
                         <ul className="flex flex-row font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 space-x-8 md:flex-row md:mt-0 md:border-0 md:bg-white  md:dark:bg-slate-900 dark:border-slate-700">
-                            {links.map(({ id, target, to }) =>
+                            {links.map(({ id, element, to }) =>
                                 <div key={id}>
                                     <NavLink to={to}>
-                                        <li className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">{target} </li>
+                                        <li className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">{element} </li>
                                     </NavLink>
                                 </div>
                             )}
@@ -57,8 +57,14 @@ function Navbar() {
                         </div>
                     </div>
 
-                    {/*         Handle humburger menu bar        */}
-                    <div onClick={() => setNav(!nav)} className="cursor-pointer pr-4 z-10 text-gray-400 md:hidden">
+                    {/*         handle mobile   ||  hamburger menu bar        */}
+                    <div onClick={() => setNav(!nav)}
+                        className="cursor-pointer pr-4  z-10 text-gray-400 md:hidden flex items-center ">
+                        <div className="px-4">
+                            <NavLink to="/register">
+                                <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-3 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-slate-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 dark:shadow-lg dark:shadow-slate-600/20">Get started</button>
+                            </NavLink>
+                        </div>
 
                         {nav ? <FaTimes size={25} /> : <FaBars size={25} />}
 
