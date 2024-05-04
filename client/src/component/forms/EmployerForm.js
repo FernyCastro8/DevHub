3be421031fe00bd12259687e12e470d1af1d5bfa
Ferny <fernc28@outlook.com>
2024-05-03 22:41:41 -0400

    employer form component - working on form input handling

--- client/src/component/forms/EmployerForm.js
+++ client/src/component/forms/EmployerForm.js
@@ -30,14 +30,20 @@ function EmployerForm() {
 
                         <div className="grid xl:grid-cols-2 xl:gap-6">
                             <div className="relative z-0 mb-6 w-full group">
-                                <input type="text" name="floating_first_name" id="floating_first_name"
+                                <input
+                                    type="text"
+                                    id="floating_first_name"
+                                    name="floating_first_name"
                                     className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                                     placeholder=" " required />
                                 <label for="floating_first_name"
                                     className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First name</label>
                             </div>
                             <div className="relative z-0 mb-6 w-full group">
-                                <input type="text" name="floating_last_name" id="floating_last_name"
+                                <input
+                                    type="text"
+                                    id="floating_last_name"
+                                    name="floating_last_name"
                                     className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                                     placeholder=" " required />
                                 <label for="floating_last_name"
@@ -45,7 +51,10 @@ function EmployerForm() {
                             </div>
 
                             <div className="relative z-0 mb-6 w-full group">
-                                <input type="text" name="floating_last_name" id="floating_last_name"
+                                <input
+                                    type="text"
+                                    id="floating_last_name"
+                                    name="floating_last_name"
                                     className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                                     placeholder=" " required />
                                 <label for="floating_last_name"
@@ -53,15 +62,21 @@ function EmployerForm() {
                             </div>
 
                             <div className="relative z-0 mb-6 w-full group">
-                                <input type="text" name="floating_last_name" id="floating_last_name"
-                                    className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                                <input
+                                    type="text"
+                                    id="floating_last_name"
+                                    name="floating_last_name"
+                                    className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                    placeholder=" " required />
                                 <label for="floating_last_name"
                                     className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Salary</label>
                             </div>
 
                             <div className="relative z-0 mb-6 w-full group">
-                                <input type="text"
-                                    name="floating_company" id="floating_company"
+                                <input
+                                    type="text"
+                                    id="floating_company"
+                                    name="floating_company"
                                     className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                                     placeholder=" " required />
                                 <label for="floating_company"
@@ -69,29 +84,40 @@ function EmployerForm() {
                             </div>
 
                             <div className="relative z-0 mb-6 w-full group">
-                                <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-                                    name="floating_phone" id="floating_phone"
+                                <input
+                                    type="tel"
+                                    pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                                    id="floating_phone"
+                                    name="floating_phone"
                                     className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                                     placeholder=" " required />
-                                <label for="floating_phone" className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone number (Ex. 123-456-7890)</label>
+                                <label for="floating_phone"
+                                    className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone number (Ex. 123-456-7890)</label>
+                            </div>
+
+                            <div className="relative z-0 mb-6 w-full group">
+                                <input
+                                    type="email"
+                                    name="floating_email"
+                                    className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                    placeholder=" " required />
+                                <label for="floating_email"
+                                    className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
                             </div>
                         </div>
 
+
                         <div className="relative z-0 mb-6 w-full group">
-                            <textarea type="text"
-                                name="floating_company" id="floating_company"
+                            <textarea
+                                type="text"
+                                id="floating_company"
+                                name="floating_company"
                                 className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                                 placeholder=" " required />
                             <label for="floating_company"
                                 className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Job Description</label>
                         </div>
 
-
-                        {/*         email imput        */}
-                        {/* <div className="relative z-0 mb-6 w-full group">
-                            <input type="email" name="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                            <label for="floating_email" className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
-                        </div> */}
 
                         {/*          password input     */}
                         {/* <div className="relative z-0 mb-6 w-full group">
