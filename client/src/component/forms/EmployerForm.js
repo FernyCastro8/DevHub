aef2cb7a2e9ec80a7724387ba1eb8bb94330631e
Ferny <fernc28@outlook.com>
2024-05-05 16:23:48 -0400

    employer form component - working on form input handling

--- client/src/component/forms/EmployerForm.js
+++ client/src/component/forms/EmployerForm.js
@@ -1,6 +1,4 @@
-import React from 'react'
-import { useState } from 'react';
-
+import React, { useState } from 'react'
 
 
 function EmployerForm() {
@@ -51,15 +49,15 @@ function EmployerForm() {
 
     return (
         <div className="h-screen w-screen bg-gray-100 pb-16 overflow-auto">
-            <section class="developer'form">
-                <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
-                    <h1 class="mb-6 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl">Discover Elite Talent for Your Projects!
+            <section className="developer'form">
+                <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
+                    <h1 className="mb-6 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl">Discover Elite Talent for Your Projects!
                     </h1>
-                    <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">🎯 Showcase Your Skills: Stand out from the crowd by displaying your expertise to our vibrant community.
+                    <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">🎯 Showcase Your Skills: Stand out from the crowd by displaying your expertise to our vibrant community.
                     </p>
-                    <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">🤝 Connect with Industry Leaders: Network with top professionals, gaining insights and mentorship to fuel your growth.
+                    <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">🤝 Connect with Industry Leaders: Network with top professionals, gaining insights and mentorship to fuel your growth.
                     </p>
-                    <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">💡 Unlock New Opportunities: Access exclusive projects and collaborations that open doors to exciting career advancements.
+                    <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">💡 Unlock New Opportunities: Access exclusive projects and collaborations that open doors to exciting career advancements.
                     </p>
                 </div>
             </section>
@@ -112,7 +110,6 @@ function EmployerForm() {
                             <div className="relative z-0 mb-6 w-full group">
                                 <input
                                     type="text"
-                                    pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
                                     id="floating_phone"
                                     name="employmentType"
                                     value={formData.employmentType}  // Value to controlled by state
@@ -126,7 +123,7 @@ function EmployerForm() {
                             <div className="relative z-0 mb-6 w-full group">
                                 <input
                                     type="tel"
-                                    pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                                    // pattern="[0-9]{1}-[0-9]{3}-[0-9]{3}-[0-9]{4}"
                                     id="floating_phone"
                                     name="phoneNumber"
                                     value={formData.phoneNumber}  // Value to controlled by state
@@ -151,7 +148,7 @@ function EmployerForm() {
 
                             <div className="relative z-0 mb-6 w-full group">
                                 <input
-                                    type="email"
+                                    type="text"
                                     name="location"
                                     className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                                     placeholder=" " required />
@@ -175,17 +172,6 @@ function EmployerForm() {
                                 className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Job Description</label>
                         </div>
 
-
-                        {/*          password input     */}
-                        {/* <div className="relative z-0 mb-6 w-full group">
-                            <input type="password" name="floating_password" id="floating_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                            <label for="floating_password" className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
-                        </div> */}
-                        {/*         Confirm password input    */}
-                        {/* <div className="relative z-0 mb-6 w-full group">
-                            <input type="password" name="repeat_password" id="floating_repeat_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                            <label for="floating_repeat_password" className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Confirm password</label>
-                        </div> */}
 
                         <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
                     </form>
